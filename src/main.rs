cb6e24d7897696329132c34cecc0e511cf67e4d7
Angelo Carly <angelo.carly@protonmail.com>
2024-09-22 19:22:20 +0200

    Rename rect attributes

--- src/main.rs
+++ src/main.rs
@@ -7,19 +7,17 @@ mod export;
 
 fn main() {
 
-    let r = Rect {
-        p1: Vec2::new(0.0, 0.0),
-        p2: Vec2::new(1.0, 1.0),
-    };
-
-    let r2 = Rect {
-        p1: Vec2::new(5.0, 10.0),
-        p2: Vec2::new(1.0, 1.0),
-    };
+    let size = Vec2::new(1.0, 1.0);
 
     let mut objects : Vec<Box<dyn Parseable>> = Vec::new();
-    objects.push(Box::new(r));
-    objects.push(Box::new(r2));
+
+    for i in 0..10 {
+        let r = Rect {
+            pos: Vec2::new(1.0, 1.0) * i as f32,
+            size: size,
+        };
+        objects.push(Box::new(r));
+    }
 
     Parser::parse(objects);
 }
--- src/shape.rs
+++ src/shape.rs
@@ -7,17 +7,17 @@ fn param(input: Vec2) -> (f32, f32) {
 }
 
 pub struct Rect {
-    pub p1: Vec2,
-    pub p2: Vec2,
+    pub pos: Vec2,
+    pub size: Vec2,
 }
 
 impl Parseable for Rect {
     fn parse(&self) -> Path {
         let data = svg::node::element::path::Data::new()
-            .move_to(param(self.p1))
-            .line_to(param(self.p1 + Vec2::new(self.p2.x, 0.0)))
-            .line_to(param(self.p1 + Vec2::new(self.p2.x, self.p2.y )))
-            .line_to(param(self.p1 + Vec2::new(0.0, self.p2.y)))
+            .move_to(param(self.pos))
+            .line_to(param(self.pos + Vec2::new(self.size.x, 0.0)))
+            .line_to(param(self.pos + Vec2::new(self.size.x, self.size.y )))
+            .line_to(param(self.pos + Vec2::new(0.0, self.size.y)))
             .close();
 
         Path::new()
