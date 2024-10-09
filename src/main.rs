247626cb2817c56b9c27d9b9d57ed9e2f8f8c60f
Angelo Carly <angelo.carly@protonmail.com>
2024-10-09 10:34:59 +0200

    Add curves

--- src/main.rs
+++ src/main.rs
@@ -6,19 +6,80 @@ use crate::shape::Exx;
 use glam::DVec2;
 use rand::random;
 
+/*
+ * Intersection of two lines functions
+ */
+fn intersect_graph_lines(a1: f64, b1: f64, a2: f64, b2: f64) -> Option<DVec2> {
+    if a1 == a2 {
+        return None
+    }
+
+    let x = (b2 - b1) / (a1 - a2);
+    let y = a1 * x + b1;
+
+    Some(DVec2::new(x, y))
+}
+
+/*
+ * Intersection of two lines defined by two points each
+ */
+fn intersect_lines(a: DVec2, b: DVec2, c: DVec2, d: DVec2) -> Option<DVec2> {
+    let a1 = (b.y - a.y) / (b.x - a.x);
+    let b1 = a.y - a1 * a.x;
+
+    let a2 = (d.y - c.y) / (d.x - c.x);
+    let b2 = c.y - a2 * c.x;
+
+    if let Some(p) = intersect_graph_lines(a1, b1, a2, b2) {
+
+        if( p.x >= f64::min(a.x,b.x) && p.x <= f64::max(a.x,b.x)
+            && p.y >= f64::min(a.y,b.y) && p.y <= f64::max(a.y,b.y)
+            && p.x >= f64::min(c.x,d.x) && p.x <= f64::max(c.x,d.x)
+            && p.y >= f64::min(c.y,d.y) && p.y <= f64::max(c.y,d.y) ) {
+
+            return Some(p)
+        }
+    }
+
+    None
+}
+
 fn main() {
     let basepos = DVec2::new(10.0, 10.0);
 
     let mut objects: Vec<Box<dyn Parseable>> = Vec::new();
 
-    let scale = 50.;
-    let exx = Exx {
-        p1: basepos + DVec2::new(random::<f64>() * scale, random::<f64>() * scale),
-        p2: basepos + DVec2::new(random::<f64>() * scale, random::<f64>() * scale),
-        p3: basepos + DVec2::new(random::<f64>() * scale, random::<f64>() * scale),
-    };
+    let scale = 100.;
+    let a = basepos + DVec2::new(random::<f64>() * 100., random::<f64>() * 100.);
+    let b = basepos + DVec2::new(random::<f64>() * 100., random::<f64>() * 100.);
+    let c = basepos + DVec2::new(random::<f64>() * 100., random::<f64>() * 100.);
+    let d = basepos + DVec2::new(random::<f64>() * 100., random::<f64>() * 100.);
 
-    objects.push(Box::new(exx));
+    if let Some(r) = intersect_lines(a, b, c, d) {
+        objects.push(Box::new(Exx{
+            p1: a,
+            p2: r,
+            p3: c
+        }));
+        objects.push(Box::new(Exx{
+            p1: b,
+            p2: r,
+            p3: d
+        }));
+    } else {
+        if let Some(r) = intersect_lines(a, c, b, d) {
+            objects.push(Box::new(Exx {
+                p1: a,
+                p2: r,
+                p3: b
+            }));
+            objects.push(Box::new(Exx {
+                p1: c,
+                p2: r,
+                p3: d
+            }));
+        }
+    }
 
     Parser::parse(objects);
 }
--- src/shape.rs
+++ src/shape.rs
@@ -1,6 +1,8 @@
+use std::fmt::format;
 use glam::{DVec2};
-use vsvg::DocumentTrait;
+use vsvg::{DocumentTrait, PathTrait};
 use vsvg::exports::kurbo;
+use vsvg::exports::kurbo::Shape;
 use crate::export::parser::Parseable;
 
 pub struct Exx {
@@ -12,24 +14,33 @@ pub struct Exx {
 impl Parseable for Exx {
     fn parse(&self, doc: &mut vsvg::Document) {
 
-        // push a path to layer 1
-        doc.push_path(1, vec![
-            (self.p1.x, self.p1.y),
-            (self.p2.x, self.p2.y),
-            (self.p3.x, self.p3.y)
-        ]);
-
         // Normals from p2 out
         let n1 = (self.p2 - self.p1).normalize();
         let n2 = (self.p2 - self.p3).normalize();
 
-        let len = 0.7;
-        let l1 = (self.p2 - self.p1).length() * len;
-        let q1 = self.p2 - n1 * l1;
+        for len in 0..20 {
+            let l1 = f64::min((self.p2 - self.p1).length(), len as f64);
+            let q1 = self.p2 - n1 * l1;
 
-        let l2 = (self.p2 - self.p3).length() * len;
-        let q2 = self.p2 - n2 * l2;
-        let path = kurbo::QuadBez::new((q1.x, q1.y), (self.p2.x, self.p2.y), (q2.x, q2.y));
-        doc.push_path(1, path)
+            let l2 = f64::min((self.p2 - self.p3).length(), len as f64);
+            let q2 = self.p2 - n2 * l2;
+
+            doc.push_path(1, vec![
+                (self.p1.x, self.p1.y),
+                (self.p2.x, self.p2.y),
+                (self.p3.x, self.p3.y),
+            ]);
+
+            let mut path = vsvg::Path::from_svg(
+                format!(
+                    "M {} {} Q {} {} {} {}",
+                    q1.x, q1.y,
+                    self.p2.x, self.p2.y,
+                    q2.x, q2.y,
+                ).as_str()).unwrap();
+            // path.metadata_mut().stroke_width = 0.1;
+
+            doc.push_path(1, path)
+        }
     }
 }
